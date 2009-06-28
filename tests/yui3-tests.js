3c6dabbb54f2b0ccdc5c7964be500cdd2aeee9ec
Eric Ferraiuolo <eferraiuolo@gmail.com>
2009-06-28 01:41:39 -0400

    revised tests for YUI3 using the Beta 1 API. Enabled the YUI3 test in the config.

--- tests/yui3-tests.js
+++ tests/yui3-tests.js
@@ -3,35 +3,30 @@ window.tests = {
 	
 	"make" : function(){	
 		var ul, i;
-		for ( i = 0; i < 250; i++ ) {
-			ul = Y.Node.create('<ul></ul>');
-			ul.addClass('fromcode');
-			ul.set('id', 'setid'+i).set('innerHTML', '<li>one</li><li>two</li><li>three</li>');
+		
+		for (i = 0; i < 250; i++){
+			ul = Y.Node.create('<ul class="fromcode"></ui>');
+			ul.set('id', 'setid'+i).append('<li>one</li><li>two</li><li>three</li>');
 			Y.get('body').appendChild(ul);
 		}
 		return Y.all('ul.fromcode').size();
 	},
 	
 	"indexof" : function(){
-		// can we do better here? This isn't pretty
-		var target, item, index, i;
-		for ( i = 0; i < 20; i++ ) {
-			item = 0;
-			index = 0;
-			target = Y.get('#setid150');
-			Y.all('ul').each(function(n){
-				if ( n.compareTo(target) ) {
-					index = item;
-				}
-				item ++;
-			});
+		var uls, index, i;
+		
+		for (i = 0; i < 20; i++){
+			index = Y.all('ul').indexOf(Y.get('#setid150'));
 		}
 		return index;
 	},
 	
 	"bind" : function(){
-		var subscriber = function(){};
-		return Y.all('ul > li').on('click', subscriber).length;
+		var subscriber = function(){},
+			lis = Y.all('ul > li');
+			
+		lis.on('click', subscriber);
+		return lis.size();
 	},
 	
 	"attr" : function(){
@@ -41,6 +36,7 @@ window.tests = {
 	"bindattr" : function(){
 		var nodes = Y.all('ul > li'),
 			subscriber = function(){};
+		
 		nodes.on('mouseover', subscriber);
 		nodes.set('rel', 'touched');
 		nodes.detach('mouseover', subscriber);
@@ -48,22 +44,22 @@ window.tests = {
 	},
 
 	"table": function(){
-		var table, i;
-		for ( i = 0; i < 40; i++ ) {
+		var body = Y.get('body'),
+			table, i;
+		
+		for (i = 0; i < 40; i++) {
 			table = Y.Node.create('<table class="madetable"></table>');
-			Y.get('body').appendChild(table);
-			table.set('innerHTML', '<tr><td>first</td></tr>');
-			table.query('td').get('parentNode').insertBefore(Y.Node.create('<td></td>'));
+			body.appendChild(table);
+			table.append('<tr><td>first</td></tr>').query('tr').prepend('<td></td>');
 		}
 		return Y.all('tr td').size();
 	},
 	
 	"addanchor" : function(){
-		return Y.all('.fromcode > li').set('innerHTML', '<a href="http://example.com">link</a>').length;
+		return Y.all('.fromcode > li').append('<a href="http://example.com">link</a>').size();
 	},
 
 	"append" : function(){
-		// Selector failing on "div[rel^='foo2']"
 		for ( var i = 0; i < 500; i++ ) {
 			Y.get('body').appendChild(Y.Node.create('<div rel="foo2"></div>'));
 		}
@@ -71,54 +67,46 @@ window.tests = {
 	},
 	
 	"addclass-odd" : function(){
-		// using ':nth-child(even)' because others start counting at 0
-		var divs = Y.all('div'),
-			odds = [],
-			i = 0;
-		divs.addClass('added');
-		divs.each(function(div){
-			if ( i % 2 === 1 ) {
-				odds.push(div.addClass('odd'));
-			}
-			i++;
-		});
-		return odds.length;
+		return Y.all('div').addClass('added').odd().addClass('odd').size();
 	},
 	
 	"style": function(){
-		return Y.all('.added').setStyles({ 'background-color':'#ededed', 'color':'#fff' }).length;
+		return Y.all('.added').setStyles({ 'background-color':'#ededed', 'color':'#fff' }).size();
 	},
 	
 	"removeclass": function(){
-		return Y.all('.added').removeClass('added').length;
+		return Y.all('.added').removeClass('added').size();
 	},
 	
 	"sethtml": function(){
-		return Y.all('div').set('innerHTML', '<p>new content</p>').length;
+		return Y.all('div').set('innerHTML', '<p>new content</p>').size();
 	},
 	
 	"insertbefore" : function(){
-		return Y.all('.fromcode a').insertBefore(Y.Node.create('<p>A Link</p>')).length;
-	},
-	
-	"insertafter" : function(){
-		return Y.all('.fromcode a').appendChild(Y.Node.create('<p>After Link</p>')).length;
-	},
-	
-	"destroy": function(){ 
-		var nodes = Y.all('.fromcode');
-		nodes.each(function(node){
-			node.get('parentNode').removeChild(node);
+		var nodes = Y.all('.fromcode a');
+		
+		nodes.each(function(){
+			this.insert(Y.Node.create('<p>A Link</p>'), this);
 		});
 		return nodes.size();
 	},
 	
+	"insertafter" : function(){
+		var nodes = Y.all('.fromcode a');
+		
+		nodes.each(function(){
+			this.insert(Y.Node.create('<p>After Link</p>'), this.next());
+		});
+		return nodes.size();
+	},
+	
+	"destroy": function(){ 
+		return Y.all('.fromcode').remove().size();
+	},
+	
 	"finale": function(){
-		var body = Y.get('body'),
-			nodes;
-		body.set('innerHTML', '');
-		nodes = Y.all('body *');
-		return (nodes ? nodes.size() : 0);
+		Y.all('body *').remove();
+		return Y.all('body *').size();
 	}
 	
 };
